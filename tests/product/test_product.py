47c103231275afc7726ea5e1c45de45b320f4468
raffrasson <raf.frasson@gmail.com>
2022-09-26 11:08:26 -0300

    feat: completes req. 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,33 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+# https://stackoverflow.com/questions/43119776/can-i-handle-multiple-asserts-within-a-single-python-pytest-method
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_mock = Product(
+        id=21,
+        nome_da_empresa="empresa",
+        nome_do_produto="produto",
+        data_de_fabricacao="21-12-2012",
+        data_de_validade="12-12-2121",
+        numero_de_serie="18462946593",
+        instrucoes_de_armazenamento="camara fria",
+    )
+
+    assert (
+        product_mock.id,
+        product_mock.nome_da_empresa,
+        product_mock.nome_do_produto,
+        product_mock.data_de_fabricacao,
+        product_mock.data_de_validade,
+        product_mock.numero_de_serie,
+        product_mock.instrucoes_de_armazenamento,
+    ) == (
+        21,
+        "empresa",
+        "produto",
+        "21-12-2012",
+        "12-12-2121",
+        "18462946593",
+        "camara fria",
+    )
