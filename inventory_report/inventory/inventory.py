529f96444ab8f4f87faad3d7822ea42312994f97
raffrasson <raf.frasson@gmail.com>
2022-09-28 12:41:32 -0300

    feat: actually completes req. 5 (it was incomplete in the last commit)

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,9 @@
 from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
+import pathlib
+import xmltodict
 
 
 class Inventory:
@@ -8,7 +12,35 @@ class Inventory:
             data = list(csv.DictReader(file))
             return data
 
+    def get_json(path):
+        with open(path, encoding="utf-8") as file:
+            data = list(json.load(file))
+            return data
+
+    def get_xml(path):
+        # ref:
+        # https://trybecourse.slack.com/archives/C01PLFW7347/p1643819956428249
+        with open(path, encoding="utf-8") as file:
+            data = xmltodict.parse(file.read())["dataset"]["record"]
+            print(data)
+            return data
+
     def import_data(path, type):
 
-        if type == "simples":
+        # ref (pesquisei como diferenciar por extensão):
+        # https://www.geeksforgeeks.org/how-to-get-file-extension-in-python/
+
+        if type == "simples" and pathlib.Path(path).suffix == ".csv":
             return SimpleReport.generate(Inventory.get_csv(path))
+        if type == "completo" and pathlib.Path(path).suffix == ".csv":
+            return CompleteReport.generate(Inventory.get_csv(path))
+
+        if type == "simples" and pathlib.Path(path).suffix == ".json":
+            return SimpleReport.generate(Inventory.get_json(path))
+        if type == "completo" and pathlib.Path(path).suffix == ".json":
+            return CompleteReport.generate(Inventory.get_json(path))
+
+        if type == "simples" and pathlib.Path(path).suffix == ".xml":
+            return SimpleReport.generate(Inventory.get_xml(path))
+        if type == "completo" and pathlib.Path(path).suffix == ".xml":
+            return CompleteReport.generate(Inventory.get_xml(path))
